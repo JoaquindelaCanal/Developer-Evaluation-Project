3c47b2a9851ea63339dd815c2ab9c2a92a1ac5f4
JoaquindelaCanal <joaquindelacanal@hotmail.com>
2025-07-02 15:33:52 -0300

    Sale and Sale Item DTO

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/DTOs/SaleDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.DTOs
+{
+    public class SaleDto
+    {
+        public Guid Id { get; set; }
+        public string SaleNumber { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public Guid BranchId { get; set; }
+        public string BranchName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+        public List<SaleItemDto> Items { get; set; }
+    }
+}
