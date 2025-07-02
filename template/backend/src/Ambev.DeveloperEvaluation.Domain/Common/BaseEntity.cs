9e500e4117374a2103f3cc799711379f5e093d7d
JoaquindelaCanal <joaquindelacanal@hotmail.com>
2025-07-01 22:23:21 -0300

    Domain Event Functionality Base Entity

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Common/BaseEntity.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Common/BaseEntity.cs
@@ -1,10 +1,26 @@
 ﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Ambev.DeveloperEvaluation.Domain.Common;
 
-public class BaseEntity : IComparable<BaseEntity>
+public abstract class BaseEntity : IComparable<BaseEntity>
 {
     public Guid Id { get; set; }
+
+    private readonly List<INotification> _domainEvents = new();
+    public IReadOnlyCollection<INotification> DomainEvents => _domainEvents.AsReadOnly();
+
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; protected set; }
+
+    protected BaseEntity()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
 
     public Task<IEnumerable<ValidationErrorDetail>> ValidateAsync()
     {
@@ -19,5 +35,67 @@ public class BaseEntity : IComparable<BaseEntity>
         }
 
         return other!.Id.CompareTo(Id);
+    }
+
+    // <summary>
+    /// Adds a domain event to be dispatched after the aggregate root is persisted.
+    /// </summary>
+    /// <param name="eventItem">The domain event to add.</param>
+    public void AddDomainEvent(INotification eventItem)
+    {
+        _domainEvents.Add(eventItem);
+    }
+
+    /// <summary>
+    /// Removes a specific domain event.
+    /// </summary>
+    /// <param name="eventItem">The domain event to remove.</param>
+    public void RemoveDomainEvent(INotification eventItem)
+    {
+        _domainEvents.Remove(eventItem);
+    }
+
+    /// <summary>
+    /// Clears all domain events from the entity. This is typically called by the dispatcher
+    /// after events have been handled.
+    /// </summary>
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is not BaseEntity other)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        if (Id.Equals(default(Guid)) || other.Id.Equals(default(Guid)))
+            return false; // Not persisted yet, so not truly equal by ID
+
+        return Id.Equals(other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(BaseEntity left, BaseEntity right)
+    {
+        if (ReferenceEquals(left, null) && ReferenceEquals(right, null))
+            return true;
+
+        if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(BaseEntity left, BaseEntity right)
+    {
+        return !(left == right);
     }
 }
