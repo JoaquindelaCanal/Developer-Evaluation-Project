ebecda4f5293361f2eb72f0038d3a13c0bd4e817
JoaquindelaCanal <joaquindelacanal@hotmail.com>
2025-07-01 23:42:50 -0300

    Create Domain Event Classes

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Events/SaleItemAddedEvent.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Domain.Events.Sales
+{
+    public record SaleItemAddedEvent(
+        Guid SaleId,
+        Guid SaleItemId,
+        Guid ProductId,
+        int Quantity,
+        decimal UnitPrice,
+        decimal DiscountPercentage
+    ) : INotification;
+}
