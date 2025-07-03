34233f1dcce8d5ebe748b1776d26a73059b1b60e
JoaquindelaCanal <joaquindelacanal@hotmail.com>
2025-07-03 12:40:50 -0300

    Sale Created Rebus Event

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Common/Contracts/Sales/SaleCreatedIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Ambev.DeveloperEvaluation.Application.Common.Contracts.Sales
+{
+    public class SaleCreatedIntegrationEvent
+    {
+        public Guid SaleId { get; set; }
+        public string SaleNumber { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public Guid BranchId { get; set; }
+        public string BranchName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+        public List<SaleItemCreatedIntegrationEventDto> Items { get; set; } = new();
+    }
+}
